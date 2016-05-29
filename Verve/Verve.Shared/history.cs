c9b5e9f1a717469df63f77fd9f1fa425e596c264
amanchopra641 <amanchopra64@gmail.com>
2016-05-29 10:55:55 +0530

    Initial_Commit

---
+++ Verve/Verve.Shared/history.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verve
+{
+    public class history
+    {
+ 
+        public string id { get; set; }
+        public string disease { get; set; }
+        public string ui { get; set; }
+        public string name { get; set; }
+        public string location { get; set; }
+    }
+}
